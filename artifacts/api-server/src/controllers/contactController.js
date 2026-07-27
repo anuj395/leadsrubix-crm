d0a3f63a37880cf0fb26288194a01e18a253679e
Dev Team <dev@digitalrubix.com>
2026-07-27 14:53:38 +0530

    issue fix contacts

--- artifacts/api-server/src/controllers/contactController.js
+++ artifacts/api-server/src/controllers/contactController.js
@@ -1,6 +1,7 @@
 const service = require('../services/contactService');
 const ExcelJS = require('exceljs');
 const mongoose = require('mongoose');
+const { convertKeysToCamelCase } = require('../services/crudFactory');
 
 // Helper to get organization name
 const getOrganizationName = async (orgId) => {
@@ -16,24 +17,6 @@ const getOrganizationName = async (orgId) => {
 
 const datesField = ["created_at", "updated_at", "next_follow_up_date_time", "dueDate", "nextFollowUp"];
 const booleanField = ['associateStatus', 'sourceStatus', 'transferStatus', 'transfer_status'];
-
-function convertKeysToCamelCase(obj) {
-  if (!obj || typeof obj !== 'object') return obj;
-  if (obj instanceof Date) return obj;
-  if (Array.isArray(obj)) {
-    return obj.map(convertKeysToCamelCase);
-  }
-  const out = {};
-  for (const [k, v] of Object.entries(obj)) {
-    if (k.startsWith('_')) {
-      out[k] = v;
-      continue;
-    }
-    const camelKey = k.replace(/_([a-z])/g, (m, letter) => letter.toUpperCase());
-    out[camelKey] = convertKeysToCamelCase(v);
-  }
-  return out;
-}
 
 exports.list = async (req, res, next) => {
   try {
